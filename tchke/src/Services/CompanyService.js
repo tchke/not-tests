28943ce49792e7e3de3e3669c13f2db5ddab1351
tcho tchke <tchk953@gmail.com>
2018-04-04 18:07:55 +0300

    Transportation layer

---
+++ tchke/src/Services/CompanyService.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+
+const mock = new MockAdapter(axios/*, { delayResponse: 2000 }*/);
+
+mock.onGet('/companies').reply(200, {
+    companies: [
+        {
+            id: 'db2969d6-f570-4224-85d9-6eeee83556a0',
+            name: 'Company 1',
+            ogrn: 'ogrnOfCompany',
+            type: 'ooo',
+            date: Date(),
+            active: true
+        },
+        {
+            id: 'f9933fac-9811-413d-b275-f5c4516ddeb8',
+            name: 'Company 2',
+            ogrn: 'ogrnOfCompany',
+            type: 'ip',
+            date: Date(),
+            active: true
+        }
+    ]
+});
+
+export default class CompanyService {
+    getCompanies() {
+        return axios.get('/companies');
+    }
+}
